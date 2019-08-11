12af000427586f6ff62d02fc21627e301131a774
marcelo-martins <marcelobmartins219@hotmail.com>
2019-08-11 01:52:50 -0300

    tudo

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-var x=9
+var pessoa1 = {
+    nome: "jorgin",
+    massa: 532,
+    altura: 1.5,
+    bmi: function(){
+        bmi = this.massa/(this.altura^2);
+        this.bmi = bmi;
+        return this.bmi;
+    }
+};
+console.log(pessoa1.bmi());
+console.log(pessoa1);
